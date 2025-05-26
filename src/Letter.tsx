0f03144e99af060fca569992803e64aae5361c52
masterpiece911 <the.masterpiece@outlook.com>
2025-05-26 20:41:28 +0200

    Reimplement animations

--- src/Letter.tsx
+++ src/Letter.tsx
@@ -1,11 +1,42 @@
+import { useRef } from "react";
 import classes from "./letter.module.css";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 type LetterProps = {
+  animate: boolean;
   letter: string | null;
 };
 
-const Letter = ({ letter }: LetterProps) => (
-  <span className={classes["letter"]}>{letter ?? "_"}</span>
-);
+const Letter = ({ letter, animate = false }: LetterProps) => {
+  const letterRef = useRef<HTMLSpanElement>(null);
+
+  // Create a unique key that changes when letter transitions from null to string
+  const transitionKey = letter === null ? "hidden" : `revealed-${letter}`;
+
+  return (
+    <TransitionGroup className={classes["letter-container"]} component="li">
+      <CSSTransition
+        key={transitionKey}
+        nodeRef={letterRef}
+        timeout={300}
+        enter={animate}
+        exit={animate}
+        appear={animate}
+        classNames={{
+          appear: classes["letter-transition-enter"],
+          appearActive: classes["letter-transition-enter-active"],
+          enter: classes["letter-transition-enter"],
+          enterActive: classes["letter-transition-enter-active"],
+          exit: classes["letter-transition-exit"],
+          exitActive: classes["letter-transition-exit-active"],
+        }}
+      >
+        <span ref={letterRef} className={classes["letter"]}>
+          {letter?.toUpperCase() ?? "_"}
+        </span>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
 
 export default Letter;
--- src/Word.tsx
+++ src/Word.tsx
@@ -1,8 +1,5 @@
 import classes from "./word.module.css";
-import letterClasses from "./letter.module.css";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Letter from "./Letter";
-import { useRef } from "react";
 import type { Play, RevealedWord } from "./useGame";
 
 type WordProps = {
@@ -10,55 +7,10 @@ type WordProps = {
   playState: Play;
 };
 
-function letterToKey(char: string | null, index: number) {
-  return `${char ?? "unrevealed"}_${index}`;
-}
-
-function Word({ revealedWord, playState }: WordProps) {
-  const refs = useRef<Map<string, HTMLElement | undefined | null>>(null);
-
-  const getRefs = () => {
-    if (!refs.current) {
-      refs.current = new Map();
-    }
-
-    return refs.current;
-  };
-
+function Word({ revealedWord }: WordProps) {
   const wordItems = revealedWord.map((char, index) => {
-    return (
-      <TransitionGroup
-        className={classes["letters"]}
-        timeout={300}
-        component="li"
-        key={letterToKey(char, index)}
-      >
-        <CSSTransition
-          nodeRef={(ref) => {
-            const refsMap = getRefs();
-            refsMap.set(letterToKey(char, index), ref);
-
-            return () => {
-              refsMap.delete(letterToKey(char, index));
-            };
-          }}
-          key={letterToKey(char, index)}
-          enter={playState !== "START"}
-          exit={playState !== "START"}
-          classNames={{
-            enter: letterClasses["letterTransitionEnter"],
-            enterActive: letterClasses["letterTransitionEnterActive"],
-            exit: letterClasses["letterTransitionExit"],
-            exitActive: letterClasses["letterTransitionExitActive"],
-          }}
-          timeout={300}
-        >
-          <Letter letter={char} />
-        </CSSTransition>
-      </TransitionGroup>
-    );
+    return <Letter key={`${char}${index}`} animate letter={char} />;
   });
-
   return <ul className={classes["letter-list"]}>{wordItems}</ul>;
 }
 
